53791ca624ee14fa0c2fa32684529be9a8ec49a9
manish-dalal <manish.dalal@daffodilsw.com>
2019-11-18 00:03:53 +0530

    Updates

---
+++ precache-manifest.59bc4bbb66115f280da7f2e6a8a6abce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbb83b44510bdcfbb7d533ef2f1b0b9",
+    "url": "/food-ordering/index.html"
+  },
+  {
+    "revision": "1e46e5f3d69546e3be5b",
+    "url": "/food-ordering/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "dd6b78f45998d5c01925",
+    "url": "/food-ordering/static/css/main.a6475b7a.chunk.css"
+  },
+  {
+    "revision": "1e46e5f3d69546e3be5b",
+    "url": "/food-ordering/static/js/2.a75e93e9.chunk.js"
+  },
+  {
+    "revision": "dd6b78f45998d5c01925",
+    "url": "/food-ordering/static/js/main.4ace8598.chunk.js"
+  },
+  {
+    "revision": "8b995ef9017d7a3518be",
+    "url": "/food-ordering/static/js/runtime-main.3f96a2b1.js"
+  }
+]);
